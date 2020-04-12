1e71fbb895300a03b08dd88933785a7c5af1ffe1
Amjad Masad <amjad.masad@gmail.com>
2020-04-12 15:55:26 +0000

    plot doesn't yield

--- basic.js
+++ basic.js
@@ -298,12 +298,14 @@ class Basic {
     }
   }
 
+  // This doesn't yield (flush) to keep graphics fast, users need to
+  // use pause to create an animation effect.
   plot(x, y, color) {
     this.assertDisplay();
     this.display.plot(x, y, color);
-    this.yield();
   }
 
+  // This yields (flush) since it's meant to update an entire "scene"
   draw(array) {
     this.assertDisplay();
 
